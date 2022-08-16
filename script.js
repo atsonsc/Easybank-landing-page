7a9465b97ded6bdba70bcbfa5b024c17d904628b
Atson Conceição <atsonsc@gmail.com>
2022-08-15 22:23:24 -0300

    finshed layout mobile in the section header.css and intro.css

---
+++ script.js
@@ -0,0 +1,13 @@
+const btnMobile = document.getElementById('btn-mobile')
+function toggleMenu(){
+
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('active');
+
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+
+
+
+    
